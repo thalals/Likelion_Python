76e60cf26801ee34358d2c7072e516dfad657d33
thalals <thalals@github.com>
2020-06-14 21:30:01 +0900

    입출력문

---
+++ 1.py
@@ -0,0 +1,10 @@
+# 나는 천재다 나는 최고다 내가 짱이다
+number = int(input("숫자를 입력하세요 >> "))
+result = 1
+print(number,"! = ", number,end='')         #end=' ' 줄바꿈 무시
+while(number>1) :
+    result = result*number
+    number =number -1
+    print(" x",number,end='')
+
+print(" = ",result)
